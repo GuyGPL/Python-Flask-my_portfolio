51afcd055ce252514e0bf71e974c24de450f5eb0
Poramut <poramutinb@hotmail.com>
2022-02-09 17:56:02 +0700

    Initial Commit3

---
+++ web_dep.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
